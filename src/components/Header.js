0c4607e3b8341580a57c208c8386ace0bd1ed9e7
BC-macair011 <mahialemu@gmail.com>
2022-01-24 11:30:04 +0200

    Refactor components

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Pokemon</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
